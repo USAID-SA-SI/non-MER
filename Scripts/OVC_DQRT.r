474f6996b5c16bce07d25139d7d1274fc291c38e
ctrapence <trapenceclement@gmail.com>
2023-07-11 11:26:57 +0200

    Added required packages

--- Scripts/OVC_DQRT.r
+++ Scripts/OVC_DQRT.r
@@ -1,4 +1,4 @@
-# Title: Level Two process
+`# Title: Level Two process
 # Author: C. Trapence
 # Purpose: Automating the process of Reporting AGYW_PREV for Inter-agency
 # Date:2023-07-04
@@ -7,13 +7,43 @@
 # Red text symbolizes comments
 
 #######################################################################################################################
-#  sources files used in the code include:                                                                            #
-#              1) Data Export from CBMIS                                                                              #
-#              2) Host Country Results DREAMS (USG) from DATIM Support                                                #
-#              3) Data Exchange Organisation Units from DATIM Support                                                 #
-#              4) Mechanisms from DATIM support                                                                       #
-#######################################################################################################################
+# #  sources files used in the code include:                                                                            #
+# #              1)Partners Google sheets                                                                             #
+# #                                                                                     #
+# #######################################################################################################################
+# 
+# #Make sure you have the following packages installed 
+# install.packages("devtools")
+# install.packages("googledrive")
+# install.packages("gargle")
+# install.packages("googlesheets4")
+# devtools::install_github("USAID-OHA-SI/glamr", build_vignettes = TRUE)
+# vignette("credential-management", package = "glamr")
+# install.packages("validate")
+# install.packages("flextable")
+# install.packages("plyr")
+# install.packages("rmarkdown")
+# install.packages("stringr")
+# install.packages("dplyr")
+# install.packages("tidyverse")
+# install.packages("tidyr")
+# install.packages("readr")
+# install.packages("readxl")
+# install.packages("glamr")
+# install.packages("purrr")
+# install.packages("data.table")
+# install.packages("splitstackshape")
+# install.packages("gophr")
+# install.packages("keyring")
+# install.packages("fs")
+# install.packages("readxl")
+# install.packages("lubridate")
+# install.packages("glue")
+# install.packages("validate")
+# install.packages("plyr")
+# install.packages("here")
 
+#Change this as per your computer.
 setwd("C:/Users/ctrapence/Documents")
 
 library(tidyverse)
@@ -33,8 +63,6 @@ library(googledrive)
 library(googlesheets4)
 library(glamr)
 library(gargle)
-
-
 
 ##Step 2: Global variables
 
@@ -90,7 +118,6 @@ NACOSA_A[columns_b]<-sapply(NACOSA_A[columns_b],as.integer)
 NACOSA_B<-read_sheet(as_sheets_id('https://docs.google.com/spreadsheets/d/1pbH6R54ioOjf-9rz12TNLJLqpx_9j-6CDQaSi3VDpIo/edit#gid=329992892'), sheet = "OVC Indicators") %>% janitor::row_to_names(1) %>% rename('10/31/2022'= `1667174400`,'11/30/2022'= `1669766400`,'12/31/2022'= `1672444800`,
 '1/31/2023'= `1675123200`,'2/28/2023'= `1677542400`,'3/31/2023'= `1680220800`,'4/30/2023'= `1682812800` ,'5/31/2023'= `1685491200`,'6/30/2023'= `1688083200` ,'7/31/2023'= `1690761600`,'8/31/2023'= `1693440000`,'9/30/2023'= `1696032000` ) %>% mutate(mech_code=unlist(mech_code))  %>% mutate_if(is.list,as.integer)
 
-
 NACOSA_All<-bind_rows(NACOSA_A,NACOSA_B)%>%  mutate_if(is.list, as.character) 
   
 
@@ -108,7 +135,7 @@ otherdisaggregate , community_status,indicator_status, period  ,missing ) %>% su
 ####FLAGGING MISSING DATRA FOR CORRECTION
 
 
-Missing_data<-OutputTableau %>% filter(missing=="Yes") %>% filter(period>=Date & period<=reporting_period,indicator_status=="Active",mech_code!=80002)
+Missing_data<-OutputTableau %>% filter(missing=="Yes") %>% filter(period>=Date & period<=reporting_period,indicator_status=="Active",mech_code!=80002) %>%   mutate(Dead_line="", Status="", Partner_Comment="", Cleared_for_analytics="")
 
 #LEVEL 2 Data checks
 
@@ -122,29 +149,28 @@ level2<-dcast(setDT(DQRT_level2),... ~ indicator,value.var = "value")%>% mutate(
                          `OVC_HIVSTAT_Test Not Required`+`OVC_HIVSTAT_Unknown_No HIV Status` )) %>% select(
    primepartner,mech_code,psnu,community,period,age,OVC_HIVSTAT_Negative :OVC_HIVSTAT ) %>% mutate(period=anydate(period)) %>% filter(period>=Date & period<=reporting_period)
   #Check 1 :This looks at instances where there number Eligible for VL is more than those receiving ART .
-  check1<-level2 %>% mutate(check1=OVC_VL_ELIGIBLE>`OVC_HIVSTAT_Positive_Receiving ART`,checkdescription="Number eligible for VL is more than those receiving ART") %>% select(primepartner,mech_code,psnu,community,period,age,`OVC_HIVSTAT_Positive_Receiving ART`,OVC_VL_ELIGIBLE ,check1,checkdescription) %>% filter(check1==TRUE)# OVC_HIVSTAT_Pos_Rec ART <18<OVC_VL_ELIGIBLE <18
+  check1<-level2 %>% mutate(check1=OVC_VL_ELIGIBLE>`OVC_HIVSTAT_Positive_Receiving ART`,checkdescription="Number eligible for VL is more than those receiving ART") %>% select(primepartner,mech_code,psnu,community,period,age,`OVC_HIVSTAT_Positive_Receiving ART`,OVC_VL_ELIGIBLE ,check1,checkdescription) %>% filter(check1==TRUE) %>% 
+    mutate(Deadline="", Status="", Partner_Comment="", Cleared_for_analytics="")# OVC_HIVSTAT_Pos_Rec ART <18<OVC_VL_ELIGIBLE <18
 
 level2<-dcast(setDT(DQRT_level2),... ~ indicator,value.var = "value") %>% mutate(OVC_HIVSTAT=
                        OVC_HIVSTAT_Negative  +`OVC_HIVSTAT_Positive_Not Receiving ART`+`OVC_HIVSTAT_Positive_Receiving ART`+
-                         `OVC_HIVSTAT_Test Not Required`+`OVC_HIVSTAT_Unknown_No HIV Status`)%>% select( primepartner,mech_code,psnu,community,period,age,OVC_HIVSTAT_Negative :OVC_HIVSTAT ) %>% mutate(period=anydate(period)) %>% filter(period>=Date & period<=reporting_period)
+                         `OVC_HIVSTAT_Test Not Required`+`OVC_HIVSTAT_Unknown_No HIV Status`)%>% select( primepartner,mech_code,psnu,community,period,age,OVC_HIVSTAT_Negative :OVC_HIVSTAT ) %>% mutate(period=anydate(period)) %>% filter(period>=Date & period<=reporting_period) %>% 
+  mutate(Deadline="", Status="", Partner_Comment="", Cleared_for_analytics="")
 
 #Check 2:HIVSTAT More that OVC COMPREHENSIVE
 check2<-level2 %>% mutate(check2=OVC_HIVSTAT>OVC_SERV_Comprehensive) %>% select(primepartner,mech_code,psnu,community,period,age,OVC_SERV_Comprehensive,OVC_HIVSTAT ,check2) %>% mutate(check_description="OVC_HIVSTAT is greater that OVC COMPREHENSIVE") %>% filter(check2==TRUE)# OVC_HIVSTAT_Pos_Rec ART <18<OVC_VL_ELIGIBLE <18
 #Check 3:OVC VLS>OVC_VLR
-check3<-level2 %>% mutate(check3=OVC_VLS>OVC_VLR ,checkdescription="# OVC_VL Suppression >OVC_VL_ELIGIBLE")%>% select(primepartner,mech_code,psnu,community,period,age,OVC_VLS,OVC_VLR ,check3,checkdescription) %>% filter(check3==TRUE)
+check3<-level2 %>% mutate(check3=OVC_VLS>OVC_VLR ,checkdescription="# OVC_VL Suppression >OVC_VL_ELIGIBLE")%>% select(primepartner,mech_code,psnu,community,period,age,OVC_VLS,OVC_VLR ,check3,checkdescription) %>% filter(check3==TRUE) %>% 
+  mutate(Deadline="", Status="", Partner_Comment="", Cleared_for_analytics="")
 
 #Check 2:HIVSTAT More that OVC COMPREHENSIVE
-check4<-level2 %>% mutate(check4=OVC_VLS>OVC_VL_ELIGIBLE )%>% select(primepartner,mech_code,psnu,community,period,age,OVC_VLS,OVC_VL_ELIGIBLE,check4) %>% filter(check4==TRUE) %>% 
-  
-mutate(Check_description=" OVC_HIVSTAT_Pos_Rec ART <18<OVC_VL_ELIGIBLE <18")
-
-
+check4<-level2 %>% mutate(check4=OVC_VLS>OVC_VL_ELIGIBLE )%>% select(primepartner,mech_code,psnu,community,period,age,OVC_VLS,OVC_VL_ELIGIBLE,check4) %>% filter(check4==TRUE) %>% mutate(Check_description=" OVC_HIVSTAT_Pos_Rec ART <18<OVC_VL_ELIGIBLE <18")%>% 
+  mutate(Deadline="", Status="", Partner_Comment="", Cleared_for_analytics="")
 
 
 #'[Final import output below]
 
-#write_csv(AGYW_DREAMS,"AGYW_PREV_Final.csv")
-
+  
 write.xlsx(check1,"C:/Users/ctrapence/Documents/Dataout/DQRT_Feedback.xlsx",  sheetName="Check1",append=TRUE)
 
 wb<-loadWorkbook("C:/Users/ctrapence/Documents/Dataout/DQRT_Feedback.xlsx")
@@ -153,21 +179,13 @@ addWorksheet(wb,"check2")
 writeData(wb,sheet="check2",x=check2)
 
 addWorksheet(wb,"check3")
-addWorksheet(wb,"check4")
 writeData(wb,sheet="check3",x=check3)
-addWorksheet(wb,"check3")
+
+addWorksheet(wb,"check4")
 writeData(wb,sheet="check4",x=check4)
 
+addWorksheet(wb,sheetName = "Missing_Data")
+writeData(wb,sheet = "Missing_Data",x=Missing_data)
 
 saveWorkbook(wb,"C:/Users/ctrapence/Documents/Dataout/DQRT_Feedback.xlsx",overwrite = T)
 
-#saveWorkbook(wb,"DQRT_Feedback.xlsx",overwrite = T)
-
-
-write.xlsx(check2,"C:/Users/ctrapence/Documents/Dataout/DQRT_Feedback.xlsx",  sheetName="Check2",append=FALSE)
-
-
-
-filename<-paste(Sys.Date(), "Missing_data", ".xlsx")
-
-openxlsx::write.xlsx(Missing_data, file.path(here("Dataout"),filename))
